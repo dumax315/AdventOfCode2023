f29a99f56bbdb5f8b8e50502d8416a6fd2d2bba5
Theo <theomhalpern@gmail.com>
2023-12-01 23:33:18 -0500

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,16 @@
+from aocd import get_data
+
+listOfLines = get_data(day=2, year=2023).split("\n")
+
+res = 0
+
+for i, line in enumerate(listOfLines):
+    temp = 0
+    print(f"line is {line}")
+
+
+    res += temp
+    
+    
+
+print(f"result = {res}")
