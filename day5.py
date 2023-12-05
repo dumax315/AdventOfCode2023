cebf5083c678281593fae54b41ec209d78d7087a
Theo <theomhalpern@gmail.com>
2023-12-05 03:11:13 -0500

    Update day5.py

--- day5.py
+++ day5.py
@@ -11,14 +11,16 @@ def rangeOverLapAndExess(seedRange, mapRange, mapsTo):
         return [seedRange[0]+dif,seedRange[1]+dif], []
     else:
         new = [i for i in seedRange]
+        new.extend([i for i in mapRange])
         new.sort()
         
         newRange = [new[1]+dif, new[2]+dif]
+
         if(seedRange[0] < mapRange[0]):
             return newRange, [[seedRange[0],mapRange[0]-1]]
         elif(seedRange[1] > mapRange[1]):
             return newRange, [[mapRange[1]+1,seedRange[1]]]
-        return newRange,[]
+        return newRange, []
 
 # f = open("day5.txt", "r").read()
 f = get_data(day=5, year=2023)
@@ -28,7 +30,7 @@ res = 0
 seedsWithoutRanges = f.split("\n")[0].split(": ")[1].split()
 seeds = []
 for sIndex in range(0,len(seedsWithoutRanges),2):
-    seeds.append([int(seedsWithoutRanges[sIndex]),int(seedsWithoutRanges[sIndex])+int(seedsWithoutRanges[sIndex+1])-1])
+    seeds.append([int(seedsWithoutRanges[sIndex]), int(seedsWithoutRanges[sIndex])+int(seedsWithoutRanges[sIndex+1])-1])
 
 
 nextGroup = []
@@ -38,25 +40,17 @@ for section in f.split("map:"):
 
         if(line == "" or (not line.split()[0].isdigit())):
             continue
-            
-        elif():
-            continue
-        else:
-            j = len(seeds)-1
-            while(j>=0):
-                newRange, ex = rangeOverLapAndExess(seeds[j], [int(line.split()[1]),int(line.split()[1]) + int(line.split()[2])-1], int(line.split()[0]))
-                if(len(newRange) > 0):
-                    foundNothing = False
+        for j in range(len(seeds)-1,-1,-1):
+            newRange, ex = rangeOverLapAndExess(
+                seeds[j], 
+                [int(line.split()[1]), int(line.split()[1]) + int(line.split()[2])-1], int(line.split()[0])
+            )
+            if(len(newRange) > 0):
+                foundNothing = False
 
-                    nextGroup.append(newRange)
-                    del seeds[j]
-                    seeds.extend(ex)
-                
-                j-=1
-
-    print(f" remaining seeds  = {len(seeds)}")
-    print(f" nextgroup  = {len(nextGroup)}")
-    print(len(nextGroup) + len(seeds))
+                nextGroup.append(newRange)
+                del seeds[j]
+                seeds.extend(ex)
     nextGroup.extend(seeds)
     seeds = nextGroup
     nextGroup = []
@@ -73,6 +67,5 @@ for seed in seeds:
         lowest = seed[0]
 print(lowest)
 
-# print(res)
 from aocd import submit
 submit(lowest, part="b", day=5, year=2023)
