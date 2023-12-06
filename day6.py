72bb0b222c7e62275a4f8f9f49c25024e87bae6c
Theo <theomhalpern@gmail.com>
2023-12-06 00:23:38 -0500

    Update day6.py

--- day6.py
+++ day6.py
@@ -1,17 +1,45 @@
 from aocd import get_data
+# listOfLines = open("day6.txt", "r").read().split("\n")
 
 listOfLines = get_data(day=6, year=2023).split("\n")
-res = 0
+res = 1
 
+def getDistance(totalTime, minDis):
+    totalTime = int(totalTime)
+    minDis = int(minDis)
+    start = -1
+    for i in range(1,totalTime):
+        tempTime = totalTime-i
+        
+        if start == -1 and tempTime * i > minDis:
+            start = i
+        elif start != -1 and tempTime * i <= minDis:
+            return [start, i-1]
 
-for i, line in enumerate(listOfLines):
-    print(f"line {i} is {line}")
+# races = []
+# for i in range(len(listOfLines[0].split()) -1):
+#     races.append([listOfLines[0].split()[i+1], listOfLines[1].split()[i+1]])
 
-
-    # res += temp
-
-print(f"result = {res}")
+# for race in races:
+#     ways = getDistance(race[0], race[1])
+#     print(race)
+#     print(ways)
+#     print(ways[1]-ways[0])
+#     res*=(ways[1]-ways[0])+1
+# print(f"result = {res}")
 
 
 # from aocd import submit
 # submit(res, part="a", day=6, year=2023)
+
+race = [55999793,401148522741405]
+# for i in range(1,race[0]):
+#     tempTime = race[0]-i
+    
+#     if tempTime * i > race[1]:
+#         print(i)
+#         break
+
+res = 47566339-8433454+1
+from aocd import submit
+submit(res, part="b", day=6, year=2023)
